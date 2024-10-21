f936ab2aad4111c558d81979b4afab13f8f2290c
Haazique Sayyed <haziq.sayyed@gmail.com>
2024-10-21 07:57:59 +0530

    authentication

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../layout";
+
+type Props = {};
+
+const SignUp = (props: Props) => {
+  return <Layout>SignUp</Layout>;
+};
+
+export default SignUp;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import {ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -26,13 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
